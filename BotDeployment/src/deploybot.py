d9321a563603707aefb2fd0f6426f2bb5698b1d2
Anish Prasad <v-aniprasad@microsoft.com>
2022-03-30 16:26:32 -0700

    Add parameters for new multitenant deployment

--- BotDeployment/src/deploybot.py
+++ BotDeployment/src/deploybot.py
@@ -2,20 +2,38 @@ import subprocess
 from subprocess import Popen, PIPE, STDOUT
 import os
 import argparse
+from xml.etree.ElementTree import register_namespace
 
 class Arguments:
-    def __init__(self, b, pwd):
-        self.botid = b
+    def __init__(self, botid, pwd, type, tenid, sku, rgname, rglocation, appname, svc_plan_name, svc_plan_location, svc_plan_sku):
+        self.botid = botid
         self.password = pwd
-
+        self.type = type
+        self.tenantid = tenid
+        self.sku = sku
+        self.rgname = rgname
+        self.rglocation = rglocation
+        self.webappname = appname
+        self.svc_plan_name = svc_plan_name
+        self.svc_plan_location = svc_plan_location
+        self.svc_plan_sku = svc_plan_sku
 
 def parse_args():
     argument_parser = argparse.ArgumentParser()
     argument_parser.add_argument("-b", "--botid", help="Unique name for the bot", required=True)
     argument_parser.add_argument("-pwd", "--password", help="Azure app password", required=True)
-    args = argument_parser.parse_args()
-    return Arguments(args.botid, args.password)
+    argument_parser.add_argument("-t", "--type", help="App Type: Multi Tenant, Single Tenant, Managed Identity", action="store_true", required=True)
+    argument_parser.add_argument("-tid", "--tenantid", help="Tenant ID. Reuqired for Single Tenant and UAMI")
+    argument_parser.add_argument("-s", "--sku", help="Bot pricing tier, F0 or S1", required=True)
+    argument_parser.add_argument("-rn", "--group-name", help="Resource group name", required=True)
+    argument_parser.add_argument("-rl", "--group-location", help="Resource group Azure region", required=True)
+    argument_parser.add_argument("-w", "--web-app-name", help="New web application name", required=True)
+    argument_parser.add_argument("-spn", "--svc-plan-name", help="Service plan name", required=True)
+    argument_parser.add_argument("-spl", "--svc-plan-location", help="Service plan Azure region", required=True)
+    argument_parser.add_argument("-sps", "--svc-plan-sku", help="Service plan pricing tier, default is S1", required=True)
 
+    args = argument_parser.parse_args()
+    return Arguments(args.botid, args.password, args.type, args.tenantid, args.sku, args.rgname, args.rglocation, args.webappname, args.svc_plan_name, args.svc_plan_location, args.svc_plan_sku)
 
 def find_az():
     cmd = 'where az'
