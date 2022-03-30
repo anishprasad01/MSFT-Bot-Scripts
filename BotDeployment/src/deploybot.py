c838f37b184519c8db9f8847deb5dba178e3404e
Anish Prasad <v-aniprasad@microsoft.com>
2022-03-30 13:04:59 -0700

    Find az and create registration
    Adds call to create bot registration and code to find the install directory for az cli on Windows.

--- BotDeployment/src/deploybot.py
+++ BotDeployment/src/deploybot.py
@@ -1,14 +1,55 @@
+import subprocess
+from subprocess import Popen, PIPE, STDOUT
 import os
 import argparse
 
-argument_parser = argparse.ArgumentParser()
-argument_parser.add_argument("-r", "--region", help="Valid Azure region", required=True)
-argument_parser.add_argument("-rg", "--resgroupname", help="Azure resource group name", required=True)
-argument_parser.add_argument("-b", "--botname", help="Unique name for the bot", required=True)
-argument_parser.add_argument("-sv", "--svcplanname", help="Service plan name", required=True)
-argument_parser.add_argument("-w", "--webappname", help="Azure web app name", required=True)
-argument_parser.add_argument("-a", "--appsvcname", help="Azure app service name", required=True)
-argument_parser.add_argument("-p", "--password", help="Azure app password", required=True)
-argument_parser.add_argument("-s", "--sku", help="Bot Pricing SKU/Tier", required=True)
+class Arguments:
+    def __init__(self, b, pwd):
+        self.botid = b
+        self.password = pwd
 
-argument_parser.parse_args()
+
+def parse_args():
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument("-b", "--botid", help="Unique name for the bot", required=True)
+    argument_parser.add_argument("-pwd", "--password", help="Azure app password", required=True)
+    args = argument_parser.parse_args()
+    return Arguments(args.botid, args.password)
+
+
+def find_az():
+    cmd = 'where az'
+    p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+    output = p.stdout.read()
+    res = output.splitlines()[1]
+    az_location = str(res)[2:len(str(res))-1]
+    return az_location
+
+def create_app_registration(args, az_location):
+    subprocess.check_call([az_location, "ad", "app", "create", "--display-name", args.botid, "--password", args.password, "--available-to-other-tenants"])
+
+# def create_resources(args):
+#     return
+
+# def build_project(args):
+#     return
+
+# def prepare_deploy(args):
+#     return
+
+# def zip_deploy(args):
+#     return
+
+def main():
+    args = parse_args()
+    az_location = find_az() 
+    os.chdir(args.path)
+    create_app_registration(args, az_location)
+    #create_resources(args)
+    #build_project(args)
+    #prepare_deploy(args)
+    #zip_deploy(args)
+
+# __name__
+if __name__=="__main__":
+    main()
--- README.md
+++ README.md
@@ -13,7 +13,7 @@ It can be invoked as you would any other Python script, such as with `python3 bu
 The following parameters must be specified when running the script (if applicable):
 
 - `-r/--release` - The version of Composer you would like to install. This directly corresponds to the tag name of the Release you want from the Composer repository. This parameter is **required**.
-- `-p/--path` - The path to the folder on your local filesystem where you would like to store the Composer files. The Composer files will be cloned into a new folder within this directoty. This parameter is **required**.
+- `-p/--path` - The path to the folder on your local filesystem where you would like to store the Composer files. The Composer files will be cloned into a new folder within this directory. This parameter is **required**.
 - `-w/--wipe` - If specified, the script will delete any existing files corresponding to the specified Composer version, if they exist. This parameter is *not* required.
-- `-v/--verbose` - If specified, error messages beyond the defualt messages provided by the script will be printed to the console. This parameter is *not* required.
+- `-v/--verbose` - If specified, error messages beyond the default messages provided by the script will be printed to the console. This parameter is *not* required.
 - `-s/--start` - If specified, wipe, clone, build, and install steps will be skipped and the script will simply change to the correct directory, start Composer, and open the browser. This parameter is *not* required.
